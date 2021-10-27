d48ba5b127618eea54053020ef361e2d950142a4
Minhang Xie <minxie97@gmail.com>
2021-10-26 21:54:32 -0700

    feature tasks complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,7 +1,5 @@
 import { Component } from 'react';
 import { HeartFill } from 'react-bootstrap-icons';
-import { Container } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
 
@@ -20,22 +18,16 @@ class HornedBeast extends Component {
 
     render() {
         return (
-            <div>
-                <Container fluid>
-                    <Row md={3}>
-                        <Col>
-                            <Card>
-                                <Card.Img src={this.props.image_url} alt={this.props.keyword} title={this.props.title} onClick={this.handleClick} />
-                                <Card.Body>
-                                    <Card.Title as="h2">{this.props.title}</Card.Title>
-                                    <Card.Text>{this.props.description}</Card.Text>
-                                    <Card.Text><HeartFill />: {this.state.numberOfLikes}</Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+            <Col>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img src={this.props.image_url} alt={this.props.keyword} title={this.props.title} onClick={this.handleClick} />
+                    <Card.Body style={{ width: '18rem' }}>
+                        <Card.Title as="h2">{this.props.title}</Card.Title>
+                        <Card.Text>{this.props.description}</Card.Text>
+                        <Card.Text><HeartFill />: {this.state.numberOfLikes}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
         )
     }
 }
